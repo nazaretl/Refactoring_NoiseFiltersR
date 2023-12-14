8d5cea2a998ab31c214465628c76ad128d7d43b7
Jiayi <jiayih@princeton.edu>
2023-12-14 00:57:18 -0500

    Tomeklinks inherit from Filter

--- src/filters/tomeklinks.py
+++ src/filters/tomeklinks.py
@@ -1,66 +1,61 @@
 import pandas as pd
 import numpy as np
+import dataclasses
 from sklearn.neighbors import NearestNeighbors
+from Filter import Filter
 
+@dataclasses.dataclass(frozen=False, eq=False, init=False)
+class Tomeklinks(Filter):
 
-# from Lusi's codes
-def getData(df):
-    X = df.iloc[:, :-1]
-    y = df.iloc[:, -1]  # assume last column is class
-    return X, y
+    def __init__(self, df, delete="major"):
+        self.data = df
+        self.X, self.y = self.__get_values_and_labels__()
+        self.delete = delete
+    
+    def __apply_filter__(self):
+        clean_ls = np.arange(len(self.data))
+        class_count = self.data.iloc[:,-1].value_counts()
+        # make sure there are only 2 classes
+        if len(class_count) != 2:
+            print(
+                f"only work on data in 2 classes. found {len(class_count)} classes instead"
+            )
+            return clean_ls,[]
+        major, minor = class_count.index
+        # return if balanced data but delete set to "major"
+        if class_count[major] == class_count[minor] and delete == "major":
+            print(
+                'balanced data so no cleaning is performed. consider setting delete to "both".'
+            )
+            return clean_ls,[]
+        # find tomeklinks
+        nns = NearestNeighbors(n_neighbors=1).fit(self.X, self.y).kneighbors()[1]
+        dictnn = {}
+        links = []
+        for i in range(len(nns)):
+            nn = nns[i][0]
+            if self.y[i] == self.y[nn]:
+                continue
+            if i in dictnn and nn == dictnn[i]:
+                links.append((i, nn))
+            dictnn[nn] = i
+        # drop tomeklinks
+        if self.delete == "major":
+            for i in range(len(links)):
+                p1,p2 = links[i]
+                if self.y[p2] == major:
+                    links[i] = (p2,p1)
+            clean_ls = np.delete(clean_ls,[pair[0] for pair in links])
+        elif self.delete == "both":
+            clean_ls = np.delete(clean_ls, sum(links,()))
+        else:
+            print('invalid deletion strategy. choose from "major" or "both"')
+            return clean_ls,links
+        return clean_ls,links
 
+    def noise_index(self):
+        self.clean_list, self.links = self.__apply_filter__()
+        return self.clean_list
 
-def tomeklinks(df, delete="major"):
-    X, y = getData(df)
-    dfclean = df.copy()
-    class_count = y.value_counts()
-    # make sure there are only 2 classes
-    if len(class_count) != 2:
-        print(
-            f"only work on data in 2 classes. found {len(class_count)} classes instead"
-        )
-        return df
-    major, minor = class_count.index
-    # return if balanced data but delete set to "major"
-    if class_count[major] == class_count[minor] and delete == "major":
-        print(
-            'balanced data so no cleaning is performed. consider setting delete to "both".'
-        )
-        return df, []
-    # find tomeklinks
-    nns = NearestNeighbors(n_neighbors=1).fit(X, y).kneighbors()[1]
-    dictnn = {}
-    links = []
-    for i in range(len(nns)):
-        nn = nns[i][0]
-        if y[i] == y[nn]:
-            continue
-        if i in dictnn and nn == dictnn[i]:
-            links.append((i, nn))
-        dictnn[nn] = i
-    # drop tomeklinks
-    if delete == "major":
-        for p1, p2 in links:
-            if y[p1] == major:
-                pmajor = p1
-            else:
-                pmajor = p2
-            dfclean.drop(df.index[pmajor], inplace=True)
-    elif delete == "both":
-        for p1, p2 in links:
-            dfclean.drop([df.index[p1], df.index[p2]], inplace=True)
-    else:
-        print('invalid deletion strategy. choose from "major" or "both"')
-        return df
-
-    return dfclean, links
-
-
-# example
-df1 = pd.DataFrame(np.random.randint(0, 70, size=(8, 2)))
-df1["label"] = [0 for _ in range(len(df1))]
-df2 = pd.DataFrame(np.random.randint(40, 100, size=(15, 2)))
-df2["label"] = [1 for _ in range(len(df2))]
-dftot = pd.concat([df1, df2], ignore_index=True)
-links, dfcleaned = tomeklinks(dftot)
-print(links)
+    def noisy_samples(self):
+        return self.data.iloc[list(sum(self.links,()))]
--- tests/test_tmklinks.py
+++ tests/test_tmklinks.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from filters import tomeklinks
+from filters.Tomeklinks import Tomeklinks
 
 
 def test1():
@@ -10,5 +10,7 @@ def test1():
     df2 = pd.DataFrame(np.random.randint(40, 100, size=(15, 2)))
     df2["label"] = [1 for _ in range(len(df2))]
     dftot = pd.concat([df1, df2], ignore_index=True)
-    dfcleaned, links = tomeklinks.tomeklinks(dftot)
-    assert links == [(10, 0), (17, 5)]
+    tmkfilter = Tomeklinks(dftot,delete='major')
+    clean = tmkfilter.noise_index()
+    assert list(clean) == list(np.delete(np.arange(len(dftot)),[10,17]))
+    assert tmkfilter.links == [(10, 0), (17, 5)]
